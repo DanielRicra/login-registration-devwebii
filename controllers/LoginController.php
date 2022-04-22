e55ea849ef47366bdcbb74fd6cf150e139903515
TSdnl13 <daniel.int@outlook.com>
2022-04-22 15:00:42 -0500

    Login controller post method

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,9 +1,36 @@
 <?php
+   require '../functions/database.php';
+
    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-      $respuesta = array();
-      $respuesta["exito"] = true;
-      $respuesta["mensaje"] = "Usuario creado con exito";
-      echo json_encode($respuesta);
+      $error = array();
+
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+
+      $query = "SELECT * FROM users WHERE email='" . $email . "'";
+      $respuesta = $db->query($query);
+      $userData = [];
+      while ($user = $respuesta->fetch_assoc()) {
+         $userData[] = $user;
+      }
+
+      if (!empty($userData)) {
+         if (password_verify($password, $userData[0]["password"])){
+            header("status: 202 Accepted");
+            echo json_encode($email);
+            exit;
+         } else {
+            $error["password"] = "Contraseña incorrecta";
+         }
+      } else {
+         $error["email"] = "Este correo electronico no existe";
+      }
+
+      header("status: 404 Not found");
+      echo json_encode($error);
+      exit;
+
    }
+
 
    
