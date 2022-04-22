c21077a3374c69ed4d08d42dc5ce435da1a615a3
TSdnl13 <daniel.int@outlook.com>
2022-04-22 15:04:51 -0500

    div for errors messages in login form inputs

--- views/login.php
+++ views/login.php
@@ -1,13 +1,15 @@
 <h1>Login</h1>
-<form method="post">
+<form method="post" id="login-form">
    <div class="field">
-      <label for="username">Usuario</label>
-      <input type="text" name="username" id="username">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email">
+      <div class="error" id="email-error"></div>
    </div>
 
    <div class="field">
       <label for="password">Password</label>
       <input type="password" name="password" id="password">
+      <div class="error" id="password-error"></div>
    </div>
 
    <div class="field">
