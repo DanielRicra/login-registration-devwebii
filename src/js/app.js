0e886173aebda7f20851027bca962d3b4058f555
TSdnl13 <daniel.int@outlook.com>
2022-04-22 15:02:27 -0500

    form submit event, and petition to server post for login

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,39 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const iniciarAplicacion = () => {
-   loginEvento();
+   loginEventos();
 }
 
-const loginEvento = () => {
-   const loginButton =  document.querySelector('#login-btn');
+const loginEventos = () => {
+   const loginForm =  document.querySelector('#login-form');
 
-   loginButton.addEventListener('click', (e) => {
+   loginForm.onsubmit = (e) => {
       e.preventDefault();
+
+      let formData = Object.fromEntries(new FormData(loginForm).entries());
+
       const httpRequest = new XMLHttpRequest();
-      const url = 'http://localhost/controllers/LoginController.php';
+      const url = 'http://localhost:8000/controllers/LoginController.php';
       httpRequest.open('post', url, true);
+      httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
       httpRequest.onload = function () {
-         if (httpRequest.status == 200) {
-            console.log(this.response);
+         if (httpRequest.status === 200 && this.getResponseHeader('status').includes('202')) {
+            const email = JSON.parse(this.response);
+            document.cookie = `email=${email}`;
+            window.location.href = "?p=inicio";
+         } else if (this.getResponseHeader('status').includes('404')) {
+            const errorResponse = JSON.parse(this.response);
+            console.log(errorResponse);
+            if (errorResponse.email) {
+               document.querySelector("#email-error").textContent = `${errorResponse.email}`;
+
+            } else if (errorResponse.password) {
+               document.querySelector("#password-error").textContent = `${errorResponse.password}`;
+            }
          }
       }
-
-      httpRequest.send();
-   });
+      httpRequest.send(`email=${formData.email}&password=${formData.password}`);
+   };
+  
 }
