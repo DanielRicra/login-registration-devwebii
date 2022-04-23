1e8296ebfe50994ffdbc73e3901ba3233562ed27
TSdnl13 <daniel.int@outlook.com>
2022-04-23 14:23:45 -0500

    link to create new one added

--- views/login.php
+++ views/login.php
@@ -1,22 +1,29 @@
-<h1>Login</h1>
-<form method="post" id="login-form">
-   <div class="field">
-      <label for="email">Email</label>
-      <input type="email" name="email" id="email">
-      <div class="error" id="email-error"></div>
-   </div>
+<div>
+   <h1>Iniciar Session</h1>
+   <form method="post" id="login-form">
+      <div class="field">
+         <label for="email">Email</label>
+         <input type="email" name="email" id="email">
+         <div class="error" id="email-error"></div>
+      </div>
 
-   <div class="field">
-      <label for="password">Password</label>
-      <input type="password" name="password" id="password">
-      <div class="error" id="password-error"></div>
-   </div>
+      <div class="field">
+         <label for="password">Password</label>
+         <input type="password" name="password" id="password">
+         <div class="error" id="password-error"></div>
+      </div>
 
-   <div class="field">
-      <button type="submit" id="login-btn">Login</button>
+      <div class="field">
+         <button type="submit" id="login-btn">LogIn</button>
+      </div>
+   </form>
+
+   <div class="links">
+      <a href="/?p=signup">Aun no tienes una cuenta? Registrate aqui</a>
    </div>
-</form>
+</div>
+
 
 <?php 
-   $script = "<script src='src/js/app.js'></script>";
+   $script = "<script src='src/js/login.js'></script>";
 ?>
