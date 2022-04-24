47ab137039ae091ebb42b699f0dcd89dbcab8667
TSdnl13 <daniel.int@outlook.com>
2022-04-23 19:52:31 -0500

    nav added, with logout option

--- views/inicio.php
+++ views/inicio.php
@@ -1,1 +1,23 @@
-<h1>Hello </h1>
+
+<nav class="nav">
+   <div class="logo">
+      <img src="src/img/spacex-logo.png" alt="">
+      <h1>Trabajo Final</h1>
+   </div>
+
+   <div class="nav__user" id="user-bar">
+      <div class="nav__user-img">
+         <img src="src/img/user.png" alt="User Profile">
+      </div>
+      <p id="user-name"></p>
+      <i class="fa-solid fa-caret-down"></i>
+
+      <div class="nav-logout hide">
+         <span id="logout">Cerrar Cesion</span>
+      </div>
+   </div>
+</nav>
+   
+<?php 
+   $script = "<script src='src/js/app.js'></script>";
+?>
